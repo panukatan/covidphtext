74a05e6098284036e8b04df1a74f4f5c6de90e96
Ernest Guevarra <ernest@guevarra.io>
2020-06-16 14:54:29 +0100

    add tests for combine_docs

---
+++ tests/testthat/test-10-combine.R
@@ -0,0 +1,45 @@
+x <- combine_docs(docs = "resolution")
+
+
+test_that("x is a tibble", {
+  expect_is(x, "tbl")
+})
+
+test_that("x has 6 columns", {
+  expect_equal(ncol(x), 6)
+})
+
+test_that("id is numeric", {
+  expect_is(x$id, "numeric")
+})
+
+test_that("date is a date", {
+  expect_is(x$date, "Date")
+})
+
+test_that("source is iatf", {
+  expect_true(all(x$source == "IATF"))
+})
+
+x <- combine_iatf()
+
+test_that("x is a tibble", {
+  expect_is(x, "tbl")
+})
+
+test_that("x has 6 columns", {
+  expect_equal(ncol(x), 6)
+})
+
+test_that("id is numeric", {
+  expect_is(x$id, "numeric")
+})
+
+test_that("date is a date", {
+  expect_is(x$date, "Date")
+})
+
+test_that("source is iatf", {
+  expect_true(all(x$source == "IATF"))
+})
+
