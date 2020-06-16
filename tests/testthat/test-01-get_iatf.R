63c1e58124195c8f4c6e543b8db66062be3f1806
Ernest Guevarra <ernest@guevarra.io>
2020-06-16 14:35:11 +0100

    add new tests for get_iatf_gazette

--- tests/testthat/test-01-get_iatf.R
+++ tests/testthat/test-01-get_iatf.R
@@ -29,6 +29,7 @@ test_that("url is a url", {
 test_that("error message shows", {
   expect_error(get_iatf_pdf(base = base, id = "1"))
   expect_error(get_iatf_pdf(base = base, id = 1))
+  expect_warning(get_iatf_pdf(base = base, id = c(1, 10)))
 })
 
 test_that("path is character", {
@@ -38,4 +39,32 @@ test_that("path is character", {
 test_that("filename is correct", {
   expect_true(stringr::str_detect(get_iatf_pdf(base = base, id = 10),
                                   pattern = "iatfResolution10.pdf"))
+})
+
+base <- "https://www.officialgazette.gov.ph/section/laws/other-issuances"
+
+x <- get_iatf_gazette(base = base, pages = 1)
+
+test_that("x is a tibble", {
+  expect_is(x, "tbl")
+})
+
+test_that("x has 7 columns", {
+  expect_equal(ncol(x), 7)
+})
+
+test_that("id is numeric", {
+  expect_is(x$id, "numeric")
+})
+
+test_that("date is a date", {
+  expect_is(x$date, "Date")
+})
+
+test_that("source is iatf", {
+  expect_true(all(x$source == "IATF"))
+})
+
+test_that("url is a url", {
+  expect_true(all(stringr::str_detect(x$url, "http")))
 })
